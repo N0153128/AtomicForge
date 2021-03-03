a054e0fa5f3429dfc7116f02054275d88b764bff
N0153128 <noisebro@hotmail.com>
2021-03-03 23:34:14 +0000

    Implemented super secret mining algorithm

--- main.py
+++ main.py
@@ -1,72 +1,12 @@
-# I'm currently trying to find what a miner could do to give Coins for
-
 from math import sqrt
+from random import randint
+import datetime
 
 
 class Coin:
 
     def __init__(self):
         self.unit = 0
-
-    @staticmethod
-    def primes():
-        result = []
-        ii = 0
-        while True:
-            prime = True
-            for i in range(2, ii):
-                if ii % i == 0:
-                    prime = False
-                    break
-            if prime:
-                result.append(ii)
-                print(ii)
-            ii += 1
-
-    @staticmethod
-    def square():
-        i = 10000
-        while True:
-            print(int(sqrt(i)))
-            i += 1
-
-    @staticmethod
-    def bubble(arr):
-        print(f'Original input data: {arr}')
-        for i in range(len(arr)):
-            for j in range(len(arr)):
-                try:
-                    if arr[j] > arr[j+1]:
-                        reserve = arr[j]
-                        arr[j] = arr[j+1]
-                        arr[j+1] = reserve
-                except IndexError:
-                    pass
-        return arr
-
-    # TODO
-    # @staticmethod
-    # def merge_sort(arr):
-
-    # TODO
-    # @staticmethod
-    # def quick_sort(arr):
-
-    # TODO
-    # @staticmethod
-    # def binary_search(arr):
-
-    # TODO
-    # @staticmethod
-    # def insertion_sort(arr):
-
-    # TODO
-    # @staticmethod
-    # def selection_sort(arr):
-
-    # TODO
-    # @staticmethod
-    # def shell_sort(arr):
 
     # TODO
     @staticmethod
@@ -78,44 +18,28 @@ class Coin:
                 print(f'Result found at: {cou}')
                 break
             else:
-                print('Not found')
+                pass
             cou += 1
 
+# decided to go with jump search + random integer
     # TODO
     @staticmethod
     def jump_search(arr, query):
-        cou = 0
-        div = 2
-
-        if len(arr) % 2 == 0:
-            if len(arr) > 10:
-                div = 8
-            elif len(arr) > 100:
-                div = 16
-            elif len(arr) > 1000:
-                div = 32
-        else:
-            if len(arr) > 10:
-                div = 10
-            elif len(arr) > 100:
-                div = 20
-            elif len(arr) > 1000:
-                div = 30
-
+        print(f'Search query: {query}')
+        div = int(sqrt(len(arr)))
         print(f'step = {div}, floor: {div//2}')
-        for i in arr[::div]:
-            print(i, cou)
+        i = 0
+        found = False
+        while not found:
             if i == query:
-                print(f'Found at {cou}, {i}')
+                print('found')
+                found = True
             elif i > query:
-                print('almost there')
-                for j in arr[cou-3:i:div//2]:
-                    print(j, arr.index(j))
+                for j in arr[i-div:i]:
                     if j == query:
-                        print(f'Found {j} at {cou}')
-                        break
-                break
-            cou += 3
+                        print(f'found {j}')
+                        found = True
+            i += div
 
     @staticmethod
     def array_generator(num):
@@ -124,6 +48,19 @@ class Coin:
             arr.append(i)
         return arr
 
-
+# THIS IS MY PLAYGROUND, DO NOT TOUCH please
+# Coin.jump_search(Coin.array_generator(10000000), 9999999)
 # print(f'Output data: {Coin.bubble(array)}')
-print(f'{Coin.jump_search(Coin.array_generator(100), 92)}')
+# print('Jump search: ')
+# start = datetime.datetime.now()
+# print(f'Started at: {start}')
+# Coin.jump_search(Coin.array_generator(50000000), 49999999)
+# elapsed = datetime.datetime.now() - start
+# print(f'ended at: {elapsed}')
+# print('==========')
+# print('Linear search:')
+# start = datetime.datetime.now()
+# print(f'Started at: {start}')
+# Coin.linear_search(Coin.array_generator(50000000), 49999999)
+# elapsed = datetime.datetime.now() - start
+# print(f'elapsed: {elapsed}')
